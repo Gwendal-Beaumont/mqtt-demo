9624a253ded617e50529fbc444e4b3bcc9ff4c09
Gwendal Beaumont <gwendal.beaumont@imt-atlantique.fr>
2025-05-22 17:01:02 +0200

    Fixes for MQTTv5

--- publisher/app.py
+++ publisher/app.py
@@ -1,11 +1,11 @@
 import time
 import paho.mqtt.client as mqtt
 
-broker = "mqtt"
+broker = "localhost"
 topic = "demo/topic"
 
-client = mqtt.Client()
-client.connect(broker, 1883, 60)
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.connect(broker)
 
 while True:
     message = "Hello from publisher!"
--- subscriber/app.py
+++ subscriber/app.py
@@ -1,10 +1,10 @@
 import paho.mqtt.client as mqtt
 
-broker = "mqtt"
+broker = "localhost"
 topic = "demo/topic"
 
 
-def on_connect(client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc, properties):
     print("Connected with result code", rc)
     client.subscribe(topic)
 
@@ -13,9 +13,9 @@ def on_message(client, userdata, msg):
     print(f"Received: {msg.payload.decode()}")
 
 
-client = mqtt.Client()
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect(broker, 1883, 60)
+client.connect(broker)
 client.loop_forever()
